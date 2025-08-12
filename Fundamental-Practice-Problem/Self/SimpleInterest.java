85b8736695c2cab5ee103c81ac3044bc197b2e00
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-08-12 22:20:31 +0530

    Create SimpleInterest.java

---
+++ Fundamental-Practice-Problem/Self/SimpleInterest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Principal: ");
+        double p = sc.nextDouble();
+        System.out.print("Rate (percent): ");
+        double r = sc.nextDouble();
+        System.out.print("Time (years): ");
+        double t = sc.nextDouble();
+        double si = (p * r * t) / 100.0;
+        System.out.println("Simple Interest = " + si);
+        sc.close();
+    }
+}
