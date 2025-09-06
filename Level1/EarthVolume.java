f7f752d4fe0c73b0047afb4c1f358ad2732815aa
Bhoomi Shivhare <shivharebhoomi07@gmail.com>
2025-09-06 10:25:16 +0530

    Initial commit of ProgrammingElements

---
+++ Level1/EarthVolume.java
@@ -0,0 +1,11 @@
+package Level1;
+public class EarthVolume {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double pi = 3.141592653589793;
+        double volumeKm3 = (4.0/3) * pi * Math.pow(radiusKm, 3);
+        double volumeMiles3 = volumeKm3 / Math.pow(1.6, 3); // convert km^3 to miles^3
+
+        System.out.println("The volume of earth in cubic kilometers is " + volumeKm3 + " and cubic miles is " + volumeMiles3);
+    }
+}
